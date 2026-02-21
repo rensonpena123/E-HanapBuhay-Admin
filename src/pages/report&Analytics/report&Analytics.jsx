43d7b4b0f09282870a8f4ffa188e5831fdee94e0
rensonpena123 <rensonpenag@gmail.com>
2026-02-21 22:40:33 +0800

    85% finished progress for reports and analytics:\ - provided a file and mock data for geographic and  perfomance analytics\ - created a separetd file for downloading data using PDF and excel format\ - install different collection like charts, pdf formatting and xlsx for excel

--- src/pages/report&Analytics/report&Analytics.jsx
+++ src/pages/report&Analytics/report&Analytics.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Building2, Briefcase, Timer, BadgeCheck } from "lucide-react";
 import StatCard from "../../components/statCard.jsx";
+import FilterBar from '../../components/filters/filterBar.jsx';
+import FilterItem from '../../components/filters/filterItem.jsx';
+import GeographicAnalytics from './geographicAnalytics/geographicAnalytics.jsx';
+import PerformanceAnalytics from './performanceAnalytics/performanceAnalytics.jsx';
+import DataExport from './dataExport.jsx';
 
 export default function ReportsAndAnalytics() {
+
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [barangay, setBarangay] = useState("Any");
+  const [employers, setEmployers] = useState("Any");
+  const [jobType, setJobType] = useState("Any");
+
+  const handleClear = () => {
+    setStartDate("");
+    setEndDate("");
+    setBarangay("Any");
+    setEmployers("Any");
+    setJobType("Any");
+  };
+
   return (
+    <> 
+    
+      {/* Stat Cards*/}
       <div className="bg-brand-dark p-6 pb-8 rounded-2xl">
         <h1 className="text-3xl font-bold text-white mb-6">Reports & Analytics</h1>
 
@@ -14,5 +37,89 @@ export default function ReportsAndAnalytics() {
           <StatCard title="Expired" value="15" icon={BadgeCheck} />
         </div>
       </div>
+
+      {/* Filter */}
+      <div className="p-6">
+          <FilterBar onClear={handleClear}>
+            
+            <div className="flex items-end gap-2">
+
+            {/* Range Date */}
+              <FilterItem label="Range Date">
+                <input 
+                  type="date" 
+                  value={startDate} 
+                  onChange={(e) => setStartDate(e.target.value)}
+                  className="bg-[#2b3a55] text-white text-sm border border-gray-600 rounded-lg px-3 py-2 w-38 outline-none focus:border-brand-yellow" 
+                />
+              </FilterItem>
+              <span className="text-brand-yellow font-bold pb-3">To</span>
+              <FilterItem label="">
+                <input 
+                  type="date" 
+                  value={endDate} 
+                  onChange={(e) => setEndDate(e.target.value)}
+                  className="bg-[#2b3a55] text-white text-sm border border-gray-600 rounded-lg px-3 py-2 w-38 outline-none focus:border-brand-yellow" 
+                />
+              </FilterItem>
+            </div>
+
+        {/* Barangay */}
+          <FilterItem label="Barangay">
+            <select 
+              value={barangay} 
+              onChange={(e) => setBarangay(e.target.value)}
+              className="bg-[#2b3a55] text-white text-sm border border-gray-600 rounded-lg px-3 py-2 w-44 outline-none focus:border-brand-yellow"
+            >
+              <option value="Any">Any</option>
+              <option value="Highway Hills">Highway Hills</option>
+              <option value="Addition Hills">Addition Hills</option>
+              <option value="Hulo">Hulo</option>
+              <option value="Plainview">Plainview</option>
+              <option value="Poblacion">Poblacion</option>
+            </select>
+          </FilterItem>
+
+          {/* Employers */}
+          <FilterItem label="Employers">
+            <select 
+              value={employers} 
+              onChange={(e) => setEmployers(e.target.value)}
+              className="bg-[#2b3a55] text-white text-sm border border-gray-600 rounded-lg px-3 py-2 w-44 outline-none focus:border-brand-yellow"
+            >
+              <option value="Any">Any</option>
+              <option value="HSI">HSI</option>
+              <option value="Mcdo">Mcdo</option>
+              <option value="Jollibee">Jollibee</option>
+              <option value="Inasal">Inasal</option>
+              <option value="Mr.DIY">Mr.DIY</option>
+            </select>
+          </FilterItem>
+
+        {/* Job Type */}
+          <FilterItem label="Job Type">
+            <select 
+              value={jobType} 
+              onChange={(e) => setJobType(e.target.value)}
+              className="bg-[#2b3a55] text-white text-sm border border-gray-600 rounded-lg px-3 py-2 w-44 outline-none focus:border-brand-yellow"
+            >
+              <option value="Any">Any</option>
+              <option value="IT">IT</option>
+              <option value="Construction">Construction</option>
+              <option value="Food Service">Food Service</option>
+              <option value="Retail">Retail</option>
+              <option value="Healthcare">Healthcare</option>
+            </select>
+          </FilterItem>
+
+          </FilterBar>
+
+          <GeographicAnalytics />
+
+          <PerformanceAnalytics />
+
+          <DataExport />
+        </div>
+    </> 
   );
 }
